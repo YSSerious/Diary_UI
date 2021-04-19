82fbb6b838e9db9352ffd4048608d58ebf89a978
Aleksei <Aleksei@gmail.com>
2021-04-19 16:06:35 +0300

    Change server endpoint to aws

--- src/services/ServiceConstants.js
+++ src/services/ServiceConstants.js
@@ -4,8 +4,8 @@ export const authHeaderName = 'user';
 const serverIp = '192.168.1.101';
 const serverPort = '8080';
 // local url
-export const serverUrl = `http://${serverIp}:${serverPort}/`;
+// export const serverUrl = `http://${serverIp}:${serverPort}/`;
 // aws url
-// export const serverUrl = `http://diaryback-env-2.eba-h9jmumuz.eu-central-1.elasticbeanstalk.com/`;
+export const serverUrl = `http://diaryback-env-2.eba-h9jmumuz.eu-central-1.elasticbeanstalk.com/`;
 
 export default serverUrl;
